19137d0179309557be8309dd15e5546ccfd76a14
kitarikes <ryosuke092413@gmail.com>
2020-03-26 17:43:44 +0900

    db_delete機能

--- flask_sample/app.py
+++ flask_sample/app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
 from flask_sample.database import init_db
 from flask_sample.models import Info
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
-ADMIN = 'admin'
-PASSWORD = '1111'
+
+ADMIN = os.environ.get("ADMIN")
+PASSWORD = os.environ.get("PASSWORD")
 
 def create_app():
   app = Flask(__name__)
@@ -17,13 +23,14 @@ def create_app():
   return app
 
 app = create_app()
-app.secret_key = "aaa"
+app.secret_key = "111111"
 db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET'])
 def home():
   return render_template('home.html', **session)
 
+# お問い合わせフォーム関連
 @app.route('/info', methods=['GET', 'POST'])
 def info():
   if request.method == 'GET':
@@ -31,28 +38,27 @@ def info():
   else:
     results = request.form
     print(results)
-    db.session.add(Info(name=results['name'], subject=results['subject'], affiliation=results['affiliation'], email=results['email'], contents=results['contents']))
+    db.session.add(Info(**results))
     db.session.commit()
     return render_template('info/finished.html', **session)
 
 @app.route('/confirm', methods=['POST'])
 def confirm():
   results = request.form
-  return render_template('info/confirm.html', 
-                          name=results['name'], 
-                          subject=results['subject'], 
-                          affiliation=results['affiliation'], 
-                          email=results['email'], 
-                          contents=results['contents'], 
-                          **session)
+  return render_template('info/confirm.html', **results, **session)
 
+
+# 認証関連
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
   if 'admin' in session:
     if session['admin']:
       infos = db.session.query(Info).all()
       dict_ = [info.get_detail() for info in infos]
-      return render_template('db_table.html', data=dict_, **session)
+      try:
+        return render_template('db_table.html', data=dict_, **session)
+      except:
+        return redirect('/info')
   else:
     return redirect('/sign_in')
 
@@ -74,3 +80,12 @@ def logout():
     # remove the username from the session if its there
     session.pop('admin', None)
     return redirect('/sign_in')
+
+# db関連
+@app.route('/delete', methods=['POST'])
+def delete():
+  results = request.form.getlist('id')
+  for x in results:
+    db.session.query(Info).filter(Info.id == int(x)).delete()
+    db.session.commit()
+  return redirect('/admin')
